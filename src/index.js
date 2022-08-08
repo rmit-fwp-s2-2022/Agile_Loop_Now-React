91dfdbe56e00a937582ecfb0650ed8ab64d6f255
s3813866 <tehyeuhaw@gmail.com>
2022-08-08 22:09:13 +1000

    Installed Chakra UI and react router 6
    On top of that, a page folder is added

--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const theme = extendTheme({ colors })
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ChakraProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ChakraProvider>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
