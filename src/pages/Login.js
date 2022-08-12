fe0abadda563910ece03df4245b6da95b74b8e00
s3719421 <s3719421@student.rmit.edu.au>
2022-08-12 19:23:57 +1000

    Completed login page layout

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -22,11 +22,11 @@ function Login() {
         <Header/>
 
         <Center minH={'70vh'}>
-            <Container maxW='md' boxShadow={'lg'} rounded={'lg'}>
-                <Box align={'center'}>
+            <Container maxW='md' boxShadow={'2xl'} rounded={'lg'} borderWidth={1}>
+                <Box align={'center'} pt={8}>
                     <Heading fontSize={'3xl'}>Log In</Heading>
                 </Box>
-
+                <form>
                 <Stack spacing={6} py={10} px={6}>
                     <FormControl isRequired>
                         <FormLabel>Email address</FormLabel>
@@ -39,7 +39,7 @@ function Login() {
                     </FormControl>
 
                     <Box pt={5}>
-                    <Button bg={'tomato'} color={'white'} _hover={{bg: 'red.500'}} minW={'100%'} >
+                    <Button type='submit' bg={'tomato'} color={'white'} _hover={{bg: 'red.500'}} minW={'100%'}>
                         Sign In
                     </Button>
 
@@ -48,6 +48,7 @@ function Login() {
                     </Text>
                     </Box>
                 </Stack>
+                </form>
             </Container>
         </Center>
     </Box>
