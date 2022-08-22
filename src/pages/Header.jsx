b236c834f622b9194e5e15f375468d8f1ae6eecb
s3813866 <tehyeuhaw@gmail.com>
2022-08-22 21:15:33 +1000

    feat(header)!: header has been styled

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,12 +1,70 @@
-import { Flex, Box, useColorMode, Center, IconButton } from "@chakra-ui/react";
+import {
+  useColorMode,
+  Flex,
+  Heading,
+  IconButton,
+  Spacer,
+  Button,
+  ButtonGroup,
+  Box,
+} from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 
 function Header() {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <>
-      <Flex color="white">
+    <div class="fpad">
+      <Flex minWidth="max-content" alignItems="center" gap="4">
+        <Box p="2">
+          <Heading pl="90px" size="md">
+            Loop-Agile-Now
+          </Heading>
+        </Box>
+        <Spacer />
+        <ButtonGroup gap="2" pr="10px">
+          <Button colorScheme="teal" variant="ghost">
+            Sign Up
+          </Button>
+          <Button colorScheme="teal" variant="ghost">
+            Log in
+          </Button>
+          <IconButton
+            variant="outline"
+            icon={colorMode === "light" ? <SunIcon /> : <MoonIcon />}
+            onClick={toggleColorMode}
+          ></IconButton>
+        </ButtonGroup>
+      </Flex>
+      {/* <div class="container-fluid">
+        <div class="header justify-content-between">
+          <div>
+            <IconButton
+              variant="outline"
+              icon={colorMode === "light" ? <SunIcon /> : <MoonIcon />}
+              onClick={toggleColorMode}
+            ></IconButton>
+          </div>
+          <div class="row">
+            <Box flex="1" h="20" bg="tomato">
+
+            </Box>
+            <h1 class="navbar-brand ">Loop-Agile-Now</h1>
+            <div>
+              <Stack direction="row" spacing={4} align="center">
+                <Button colorScheme="teal" variant="ghost">
+                  Login
+                </Button>
+                <Button colorScheme="teal" variant="ghost">
+                  SignUp
+                </Button>
+              </Stack>
+            </div>
+          </div>
+        </div>
+      </div> */}
+
+      {/* <Flex color="white">
         <Center w="80px" bg="tomato">
           <IconButton
             variant="outline"
@@ -23,13 +81,13 @@ function Header() {
               height: "10vh",
             }}
           >
-            <h1> LAN </h1>
+            <h1> Loop-Agile-Now</h1>
           </div>
         </Box>
       </Flex>
 
-      <h1>hello</h1>
-    </>
+      <h1>hello</h1> */}
+    </div>
   );
 }
 export default Header;
