2cce77ca727e16f4341627a0e04aa4150d957e9e
s3813866 <tehyeuhaw@gmail.com>
2022-08-24 01:13:07 +1000

    feat(header): Header buttons added navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,28 +9,37 @@ import {
   Box,
 } from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { Link } from "react-router-dom";
 // import { useState } from "react";
 
 function Header() {
   const { colorMode, toggleColorMode } = useColorMode();
 
-  // const [navColor, setNavColor] = useState(false);
   return (
     <div class="fpad">
       <Flex minWidth="max-content" alignItems="center" gap="4">
         <Box p="2">
-          <Heading pl="90px" size="md">
-            Loop-Agile-Now
-          </Heading>
+          <Link to="/">
+            <Button colorScheme="teal" variant="link">
+              <Heading pl="90px" size="md">
+                Loop-Agile-Now
+              </Heading>
+            </Button>
+          </Link>
         </Box>
         <Spacer />
         <ButtonGroup gap="2" pr="10px">
-          <Button colorScheme="teal" variant="ghost">
-            Sign Up
-          </Button>
-          <Button colorScheme="teal" variant="ghost">
-            Log in
-          </Button>
+          <Link to="/signup">
+            <Button colorScheme="teal" variant="ghost">
+              Sign Up
+            </Button>
+          </Link>
+          <Link to="/login">
+            <Button colorScheme="teal" variant="ghost">
+              Log in
+            </Button>
+          </Link>
+
           <IconButton
             variant="outline"
             icon={colorMode === "light" ? <SunIcon /> : <MoonIcon />}
